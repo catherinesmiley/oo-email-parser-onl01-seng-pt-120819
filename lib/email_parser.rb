b99272956074498e8f6a43be830212e10bee90d1
Catherine Smiley <catherinesmiley@gmail.com>
2020-01-30 01:54:21 +0000

    Automatically backed up by Learn

--- lib/email_parser.rb
+++ lib/email_parser.rb
@@ -6,19 +6,15 @@ require 'pry'
 
 class EmailAddressParser
   
-<<<<<<< HEAD
   attr_accessor :emails
-  
-=======
->>>>>>> 2ba3b7272181579e134b02e446d4db8879a0cb0f
+
   def initialize(emails)
     @emails = emails 
   end
   
-<<<<<<< HEAD
   def parse
     @emails.split(/[, ]/).reject {|string| string.empty?}.uniq
-=======
+
   def emails
     @emails 
   end
